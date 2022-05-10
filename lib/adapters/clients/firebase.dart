57f84793b068381dae43359450c9bdd1ff8e6407
Ladino Anselmo <56450813+layndev@users.noreply.github.com>
2022-05-10 22:42:27 +0200

    solving issues on firebase initialyzer

--- lib/adapters/clients/firebase.dart
+++ lib/adapters/clients/firebase.dart
@@ -4,9 +4,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
 Future<void> initialyzeFirebase() async {
-    if (!kIsWeb && !Platform.isMacOS) {
-    await Firebase.initializeApp();
-  } else if (Platform.isMacOS) {
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyDJ4BVZTJv6hPJBg9PUmjS6XZBc7SmX8BM",
+        authDomain: "petroplay-stp.firebaseapp.com",
+        projectId: "petroplay-stp",
+        storageBucket: "petroplay-stp.appspot.com",
+        messagingSenderId: "558302509436",
+        appId: "1:558302509436:web:5c34b26d3f9af2471d4cf9",
+        measurementId: "G-HVJQFXQWMS",
+      ),
+    );
+  } else if (Platform.isMacOS || Platform.isWindows) {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
         apiKey: "AIzaSyDJ4BVZTJv6hPJBg9PUmjS6XZBc7SmX8BM",
@@ -19,16 +30,6 @@ Future<void> initialyzeFirebase() async {
       ),
     );
   } else {
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyDJ4BVZTJv6hPJBg9PUmjS6XZBc7SmX8BM",
-        authDomain: "petroplay-stp.firebaseapp.com",
-        projectId: "petroplay-stp",
-        storageBucket: "petroplay-stp.appspot.com",
-        messagingSenderId: "558302509436",
-        appId: "1:558302509436:macos:5c34b26d3f9af2471d4cf9:web:5c34b26d3f9af2471d4cf9",
-        measurementId: "G-HVJQFXQWMS",
-      ),
-    );
+    await Firebase.initializeApp();
   }
 }
--- lib/pages/reception_checklists/widgets/vehicle_details_dialog.dart
+++ lib/pages/reception_checklists/widgets/vehicle_details_dialog.dart
@@ -48,154 +48,150 @@ class VehicleDetailsDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SizedBox(
-            width: isTablet ? MediaQuery.of(context).size.width / 4 : MediaQuery.of(context).size.width * .5,
-            height: MediaQuery.of(context).size.height,
-            child: Card(
-              child: Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        // imagem
-                        Center(child: Image.asset('assets/img/Model3.png', width: 160)),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: isTablet ? MediaQuery.of(context).size.width / 4 : MediaQuery.of(context).size.width * .5,
+          height: MediaQuery.of(context).size.height,
+          child: Card(
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0),
+                  child: ListView(
+                    children: [
+                      // imagem
+                      Center(child: Image.asset('assets/img/Model3.png', width: 160)),
 
-                        // placa
-                        Align(
-                          child: Column(
-                            children: [
-                              Container(alignment: Alignment.center, width: 80, height: 10, color: Colors.blue.shade900),
-                              Container(
-                                width: 80,
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(border: Border.all(width: .5, color: Colors.black87)), 
-                                child: Text(placa ?? 'No Plate'),
-                              ),
-                            ],
-                          ),
-                        ),
-
-                        SizedBox(height: 10),
-
-                        // status
-                        Align(
-                          child: Container(
-                            padding: EdgeInsets.all(5),
-                            decoration: BoxDecoration(
-                              color: Colors.orange.shade900.withOpacity(.2),
-                              borderRadius: BorderRadius.circular(6),
+                      // placa
+                      Align(
+                        child: Column(
+                          children: [
+                            Container(alignment: Alignment.center, width: 80, height: 10, color: Colors.blue.shade900),
+                            Container(
+                              width: 80,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(border: Border.all(width: .5, color: Colors.black87)), 
+                              child: Text(placa ?? 'No Plate'),
                             ),
-                            child: Text('Aguardando', style: TextStyle(color: Colors.orange.shade900)),
-                          ),
+                          ],
                         ),
+                      ),
 
-                        Divider(),
+                      SizedBox(height: 10),
 
-                        _buildSectionTile('Informações do Cliente'),
+                      // status
+                      Align(
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            color: Colors.orange.shade900.withOpacity(.2),
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: Text('Aguardando', style: TextStyle(color: Colors.orange.shade900)),
+                        ),
+                      ),
 
+                      Divider(),
+
+                      _buildSectionTile('Informações do Cliente'),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'Nome Completo', 
+                          hintText: 'digite o nome',
+                          textController: _nomeEditingController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'Celular', 
+                          hintText: '(15) 9966-5259',
+                          textController: _celularEditingController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'e-Mail', 
+                          hintText: 'teste@teste.com.br',
+                          textController: _emailEditingController,
+                        ),
+                      ),
+
+                      Divider(),
+
+                      _buildSectionTile('Informações do Veículo'),
+
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'Modelo', 
+                          hintText: 'Modelo S',
+                          textController: _modeloEditingController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'Versão / Ano', 
+                          hintText: 'Long Range / 2021',
+                          textController: _anoEditingController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'Cor', 
+                          hintText: 'Azul Metálico',
+                          textController: _corEditingController,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        child: _buildFormField(
+                          label: 'Kilometragem', 
+                          hintText: '10.000 KM',
+                          textController: _kmEditingController,
+                        ),
+                      ),
+                      if (isAuditPage)  
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
                           child: _buildFormField(
-                            label: 'Nome Completo', 
-                            hintText: 'digite o nome',
-                            textController: _nomeEditingController,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                          child: _buildFormField(
-                            label: 'Celular', 
-                            hintText: '(15) 9966-5259',
-                            textController: _celularEditingController,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                          child: _buildFormField(
-                            label: 'e-Mail', 
-                            hintText: 'teste@teste.com.br',
-                            textController: _emailEditingController,
+                            label: 'Mecânico Responsável', 
+                            hintText: 'Carlos Bergano', 
+                            textController: _mecanicoEditingController,
                           ),
                         ),
 
-                        Divider(),
+                      SizedBox(height: 20),
 
-                        _buildSectionTile('Informações do Veículo'),
+                      _buildActionButton('Iniciar Entrada', onPressed: () {}),
+                      if (!isAuditPage) SizedBox(height: 10),
+                      if (!isAuditPage) _buildActionButton('Desistência', bordered: true, onPressed: () {}),
 
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                          child: _buildFormField(
-                            label: 'Modelo', 
-                            hintText: 'Modelo S',
-                            textController: _modeloEditingController,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                          child: _buildFormField(
-                            label: 'Versão / Ano', 
-                            hintText: 'Long Range / 2021',
-                            textController: _anoEditingController,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                          child: _buildFormField(
-                            label: 'Cor', 
-                            hintText: 'Azul Metálico',
-                            textController: _corEditingController,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                          child: _buildFormField(
-                            label: 'Kilometragem', 
-                            hintText: '10.000 KM',
-                            textController: _kmEditingController,
-                          ),
-                        ),
-                        if (isAuditPage)  
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-                            child: _buildFormField(
-                              label: 'Mecânico Responsável', 
-                              hintText: 'Carlos Bergano', 
-                              textController: _mecanicoEditingController,
-                            ),
-                          ),
-
-                        SizedBox(height: 20),
-
-                        _buildActionButton('Iniciar Entrada', onPressed: () {}),
-                        if (!isAuditPage) SizedBox(height: 10),
-                        if (!isAuditPage) _buildActionButton('Desistência', bordered: true, onPressed: () {}),
-
-                        SizedBox(height: 20),
-                      ],
-                    ),
+                      SizedBox(height: 20),
+                    ],
                   ),
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    child: IconButton(
-                      onPressed: () => Navigator.of(context).pop(), 
-                      icon: Icon(Icons.clear)
-                    ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(), 
+                    icon: Icon(Icons.clear)
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
       
   }
